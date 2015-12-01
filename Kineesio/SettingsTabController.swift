fb3be630d86ad63fae1a42b4d9c401c5cc78e118
Adhit Ramamurthi <adhit.ramamurthi@gmail.com>
2015-12-01 14:42:15 -0500

    Added files

---
+++ Kineesio/SettingsTabController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsTabController.swift
+//  Kineesio
+//
+//  Created by Adhit Ramamurthi on 28.11.15.
+//  Copyright © 2015 Adhit Ramamurthi. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTabController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set nav view title to image
+        let TitleLogo = UIImage(named: "Settings Title.pdf")
+        let TitleimageView = UIImageView(image:TitleLogo)
+        self.navigationItem.titleView = TitleimageView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
