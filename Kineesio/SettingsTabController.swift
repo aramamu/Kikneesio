67303b7b27d27270df8367c93598a0af830276b5
Adhit Ramamurthi <adhit.ramamurthi@gmail.com>
2015-12-09 13:17:39 -0500

    Bluetooth
    Can discover and connect to devices

--- Kineesio/SettingsTabController.swift
+++ Kineesio/SettingsTabController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class SettingsTabController: UIViewController {
-
+class SettingsTabController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var bleManager: CBCentralManager!
+    var blePeripheral: CBPeripheral!
+    var rssiTimer = NSTimer()
+    
+    @IBOutlet weak var stopScanningButton: UIButton!
+    @IBOutlet weak var bleStatusLabel: UILabel!
+    @IBOutlet weak var startScanningButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    
+    override func viewWillAppear(animated: Bool) {
+        self.disconnectButton.hidden = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +30,8 @@ class SettingsTabController: UIViewController {
         let TitleLogo = UIImage(named: "Settings Title.pdf")
         let TitleimageView = UIImageView(image:TitleLogo)
         self.navigationItem.titleView = TitleimageView
+        bleManager = CBCentralManager()
+        bleManager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +39,56 @@ class SettingsTabController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func startScanningBLE(sender: UIButton) {
+        if (bleManager.state == .PoweredOff) {
+            self.bleStatusLabel.text = "Bluetooth is Turned Off"
+        }
+        else if (bleManager.state == .PoweredOn) {
+            self.bleStatusLabel.text = "Scanning for connections..."
+            
+            bleManager.scanForPeripheralsWithServices(nil, options: nil)
+        }
+        
+    }
+    
+    @IBAction func stopScanningBLE(sender: UIButton) {
+        bleManager.stopScan()
+        self.bleStatusLabel.text = "Stopped Scanning"
+    }
+    
+    
+    @IBAction func disconnectBLE(sender: UIButton) {
+        bleManager.cancelPeripheralConnection(blePeripheral)
+        self.disconnectButton.hidden = true
+        self.startScanningButton.hidden = false
+        self.stopScanningButton.hidden = false
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+    }
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        print("Discovered \(peripheral.name)")
+        
+        if let deviceName = peripheral.name {
+            if deviceName == "Kikneesio"
+            {
+                blePeripheral = peripheral
+                bleManager.stopScan()
+                print(blePeripheral.name!)
+                bleManager.connectPeripheral(blePeripheral, options: nil)
+            }
+        }
+    }
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        self.bleStatusLabel.text = "Connected to \(peripheral.name!)"
+        self.startScanningButton.hidden = true
+        self.stopScanningButton.hidden = true
+        self.disconnectButton.hidden = false
+    }
+    
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        self.bleStatusLabel.text = "Disconnected From Bluetooth"
+    }
 
     /*
     // MARK: - Navigation
